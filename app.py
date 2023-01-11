6d7d429f2b2d8117ccb8ffefb65b5638a56a72fd
Jonathan Gatz <J.Gatz712@gmail.com>
2023-01-11 13:14:04 -0500

    clean up app

--- app.py
+++ app.py
@@ -17,30 +17,10 @@ class Solution(object):
 
 
 
-class Solution(object):
-        def twoSum(self, nums, target):
-            complementMap = dict()
-
-            for i in range(len(nums)):
-                num = nums[i]
-                complement = target - num 
-
-                if num in complementMap:
-                    return [complementMap[num], i ]
-                else: 
-                    complementMap[complement] = i
-
-
-
-
-
-
-
-        def brutForce(self, nums, target):
-
-            for i in range(len(nums)):
-                for j in range(i+1, len(nums)):
-                    sum - nums[i] + nums[j]
-            if sum == target: 
-                return [i, j]
+#           def brutForce(self, nums, target):
+#            for i in range(len(nums)):
+#                for j in range(i+1, len(nums)):
+#                    sum - nums[i] + nums[j]
+#            if sum == target: 
+#                return [i, j]
 
